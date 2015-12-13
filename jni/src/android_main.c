cdfc7325140ba4422fdce92eb18bc156d3ae4b23
kari.zhang <kari.zhang@qq.com>
2015-12-13 21:46:57 +0800

    Debug native activity

--- jni/src/android_main.c
+++ jni/src/android_main.c
@@ -9,6 +9,7 @@
 
 #include <android/log.h>
 #include <android_native_app_glue.h>
+#include <stdlib.h>
 #include <time.h>
 #include "imgsdk.h"
 
@@ -21,10 +22,12 @@ static void HandleCommand(struct android_app *pApp, int32_t cmd) {
 	switch ( cmd )
 	{
 		case APP_CMD_SAVE_STATE:
+            LogD("save state");
 			// the OS asked us to save the state of the app
 			break;
 
         case APP_CMD_INIT_WINDOW:
+            LogD("init window");
             setEglNativeWindow(sdk, pApp->window);
             if (initSdkEnv(sdk) < 0) {
                 LogE("Failed initSdkEnv\n");
@@ -33,13 +36,17 @@ static void HandleCommand(struct android_app *pApp, int32_t cmd) {
 			break;
 
 		case APP_CMD_TERM_WINDOW:
+            LogD("term window");
 			onSdkDestroy(sdk);
 			break;
 
 		case APP_CMD_LOST_FOCUS:
+            LogD("lost focus");
 			break;
 
 		case APP_CMD_GAINED_FOCUS:
+            LogD("gain focus");
+            onSdkDraw(sdk);
             swapEglBuffers(sdk);
 			break;
 	}
@@ -77,11 +84,6 @@ void android_main(struct android_app *pApp)
 				return;
 			}
 		}
-
-        /*
-		onSdkDraw(sdkEnv);
-		swapEglBuffers(sdkEnv);
-        */
 
 	}
 }
